c0d117209dbea0be3c800a99c0346e38d840e6df
Kristjan Sigurdsson <krsigurdsson@gmail.com>
2023-05-15 12:15:57 +0200

    Add files via upload

---
+++ Take Two Stones/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace Take_Two_Stones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            long stones = 0;
+            while ((line = Console.ReadLine()) != null)
+            {
+                try
+                {
+                    stones = long.Parse(line);
+                    if (stones % 2 == 0)
+                    {
+                        Console.WriteLine("Bob");
+                    }
+                    else { Console.WriteLine("Alice"); }
+                }
+                catch { }
+            }
+        }
+    }
+}
