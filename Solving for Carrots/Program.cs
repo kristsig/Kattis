6b0a477139e60802dc892b32e8d7d7084347b7e2
Kristjan Sigurdsson <krsigurdsson@gmail.com>
2023-05-15 12:15:30 +0200

    Add files via upload

---
+++ Solving for Carrots/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace Solving_for_Carrots
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "";
+            line = Console.ReadLine();
+            string[] split = line.Split(new char[] { ' ' }, StringSplitOptions.None);
+            if (split.Length >= 2) {
+                double N = double.Parse(split[0]);
+                double P = double.Parse(split[1]);
+                for (int i = 0; i < N; i++) {
+                    Console.ReadLine();
+                }
+                Console.WriteLine(P);
+            }
+        }
+    }
+}
