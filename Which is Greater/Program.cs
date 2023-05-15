eb5c1fb9db7db19077482b255cec9aa5945e1585
Kristjan Sigurdsson <krsigurdsson@gmail.com>
2023-05-15 12:16:47 +0200

    Add files via upload

---
+++ Which is Greater/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Which_is_Greater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                try
+                {
+                    string[] split = line.Split(new char[] { ' ' }, StringSplitOptions.None);
+                    long a = long.Parse(split[0]);
+                    long b = long.Parse(split[1]);
+                    if (a > b)
+                    {
+                        Console.WriteLine(1);
+                    }
+                    else
+                    {
+                        Console.WriteLine(0);
+                    }
+                }
+                catch { }
+            }
+        }
+    }
+}
