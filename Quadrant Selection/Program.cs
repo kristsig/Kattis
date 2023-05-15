c4c40429cee04425d85b99654c5fe367e27312ab
Kristjan Sigurdsson <krsigurdsson@gmail.com>
2023-05-15 12:14:52 +0200

    Add files via upload

---
+++ Quadrant Selection/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace Quadrant_Selection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+
+            if (x > 0 && y > 0) { Console.WriteLine(1); }
+            if (x < 0 && y > 0) { Console.WriteLine(2); }
+            if (x < 0 && y < 0) { Console.WriteLine(3); }
+            if (x > 0 && y < 0) { Console.WriteLine(4); }
+        }
+    }
+}
