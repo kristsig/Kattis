b3feb197834e44565fe2db5ca9edd14b5c8770ab
Kristjan Sigurdsson <krsigurdsson@gmail.com>
2023-05-15 12:23:54 +0200

    Add files via upload

---
+++ Autori/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace Autori
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                string acronym = "";
+                string[] split = line.Split(new char[] { '-' }, StringSplitOptions.None);
+                if (split.Length > 0)
+                {
+                    for (int i = 0; i < split.Length; i++)
+                    {
+                        acronym += split[i][0];
+                    }
+                }
+                Console.WriteLine(acronym);
+            }
+        }
+    }
+}
