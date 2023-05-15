3fc4a2dcd2521c846fd91607a3876e185f6459fe
Kristjan Sigurdsson <krsigurdsson@gmail.com>
2023-05-15 12:12:31 +0200

    Add files via upload

---
+++ A Different Problem/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace A_Different_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                int sum = 0;
+                List<double> numbers = line.Split(' ').Select(double.Parse).ToList();
+                numbers.Sort();
+                Console.WriteLine(numbers[1] - numbers[0]);
+            }
+        }
+    }
+}
