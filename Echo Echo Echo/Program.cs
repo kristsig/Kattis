552d87dea53fbcd27fefcf1bd1ddf0875ef31971
Kristjan Sigurdsson <krsigurdsson@gmail.com>
2023-05-15 12:13:54 +0200

    Add files via upload

---
+++ Echo Echo Echo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace EchoEchoEcho
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            Console.WriteLine(line + " " + line + " " + line);
+        }
+    }
+}
