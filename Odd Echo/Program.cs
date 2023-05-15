04cb3fc4d615084e36b7e0409d4d6e8587f54585
Kristjan Sigurdsson <krsigurdsson@gmail.com>
2023-05-15 12:14:31 +0200

    Add files via upload

---
+++ Odd Echo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Odd_Echo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> echo = new List<string>();
+            double N = double.Parse(Console.ReadLine());
+            for (int i = 0; i < N; i++)
+            {
+                string line = Console.ReadLine();
+                if (i % 2 == 0)
+                {
+                    echo.Add(line);
+                }
+            }
+            for(int i = 0; i < echo.Count; i++)
+            {
+                Console.WriteLine(echo[i]);
+            }
+        }
+    }
+}
