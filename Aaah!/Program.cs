e239523b76e8b2c2404e0e7522abe7d510129fe7
Kristjan Sigurdsson <krsigurdsson@gmail.com>
2023-05-15 12:12:41 +0200

    Add files via upload

---
+++ Aaah!/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace Aaah_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string aOftheDay = Console.ReadLine();
+            string aRequiredbydoctor = Console.ReadLine();
+
+            if (aOftheDay.Length >= aRequiredbydoctor.Length)
+            {
+                Console.WriteLine("go");
+            }
+            else { Console.WriteLine("no"); }
+        }
+    }
+}
