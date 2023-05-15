df1e8e1fa109e388ef5786770940ee9ca8cdddd6
Kristjan Sigurdsson <krsigurdsson@gmail.com>
2023-05-15 12:14:22 +0200

    Add files via upload

---
+++ Jack-O'-Lantern Juxtaposition/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace Jack_O__Lantern_Juxtaposition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                double sum = 1;
+                string[] split = line.Split(new char[] { ' ' }, StringSplitOptions.None);
+                if (split.Length > 0)
+                {
+                    sum = double.Parse(split[0]);
+                    for(int i = 1; i < split.Length; i++)
+                    {
+                        sum = (sum * double.Parse(split[i]));
+                    }
+                }
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
