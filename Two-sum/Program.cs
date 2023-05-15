df10294ebed3f527dbb0514233f5cf8900453505
Kristjan Sigurdsson <krsigurdsson@gmail.com>
2023-05-15 12:16:35 +0200

    Add files via upload

---
+++ Two-sum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace Two_sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                int sum = 0;
+                string[] split = line.Split(new char[] { ' ' }, StringSplitOptions.None);
+                for (int i = 0; i < split.Length; i++)
+                {
+                    sum += int.Parse(split[i]);
+                }
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
