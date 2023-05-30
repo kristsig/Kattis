d4dae8a8a23a9f320485808ce692e3722b0af1da
Kristjan Sigurdsson <krsigurdsson@gmail.com>
2023-05-30 13:05:52 +0200

    Add files via upload

---
+++ 3D Printed Statues/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _3D_Printed_Statues
+{
+    internal class Program
+    {
+        static int Counting(int n)
+        {
+            int counter = 0;
+            while (n > 0)
+            {
+                counter++;
+                n >>= 1;
+            }
+            return counter - 1;
+        }
+        static int Days(int n)
+        {
+            return n < 4 ? n : (n & (n - 1)) != 0 ? Counting(n << 1 + 1) : Counting(n) + 1;
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Days(n));
+        }
+    }
+}
